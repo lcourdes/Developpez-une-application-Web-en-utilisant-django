3427688c3254d950e7efd3e70092ff18ca7f5e7f
lcourdes <lea.courdes.murphy@gmail.com>
2023-01-16 17:35:14 +0100

    Authentification et création de compte

--- LITReview/LITReview/settings.py
+++ LITReview/LITReview/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'LITReview.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.joinpath('templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = LOGIN_URL
--- LITReview/LITReview/urls.py
+++ LITReview/LITReview/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import authentication.views
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', authentication.views.login_page, name='login'),
+    path('logout/', authentication.views.logout_user, name='logout'),
+    path('signup/', authentication.views.signup_page, name='signup'),
+
+    path('home/', blog.views.home, name='home'),
 ]
