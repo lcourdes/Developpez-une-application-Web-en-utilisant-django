39aec7fbca78580fe4a48803f5943afe9323e0d2
lcourdes <lea.courdes.murphy@gmail.com>
2023-01-18 12:37:26 +0100

    Ajout du style pour l'authentification

--- LITReview/authentication/forms.py
+++ LITReview/authentication/forms.py
@@ -4,11 +4,21 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=30, label='Nom d\'utilisateur')
-    password = forms.CharField(max_length=30, widget=forms.PasswordInput, label='Mot de passe')
+    username = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Nom d\'utilisateur'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Mot de passe'}), label='')
 
 
 class SignupForm(UserCreationForm):
+    username = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Nom d\'utilisateur'}))
+    password1 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder': 'Mot de passe'}))
+    password2 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder': 'Confirmer mot de passe'}))
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ('username', )
+        fields = ['username', 'password1', 'password2']
+        help_texts = {
+            'username': None,
+            'password1': None,
+            'password2': None,
+        }
+    
+    
